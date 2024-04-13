30dfb77b3b5504db3f4c6e3dcc1d7f9b2474cf8d
guilherme-moura95 <junior.moura19@hotmail.com>
2024-04-13 17:04:42 +0100

    bugfix:langcore error

--- src/main.py
+++ src/main.py
@@ -4,45 +4,46 @@ import streamlit as st
 from langchain_community.utilities.sql_database import SQLDatabase
 from langchain_core.messages import AIMessage, HumanMessage
 import sqlalchemy
-
 import langchain as langchain
+
+# Fix langchain core Error
+langchain.verbose = False
+langchain.debug = False
+langchain.llm_cache = False
 
 
 class ChatInterface:
     def __init__(self):
         load_dotenv()
         self.chat_history = [
-            AIMessage(content="Nao Esqueca de Configurar o Banco de Dados na lateral esquerda"),
+            AIMessage(content="Não esqueça de configurar o Banco de Dados na lateral esquerda"),
             AIMessage(content="Pergunte-me qualquer coisa sobre seu banco de dados")
         ]
         st.set_page_config(page_title="Converse com seu Banco de Dados ")
         st.title('Converse com seu Banco de Dados')
-        self.db = None
-        self.db_uri = ""
-        self.engine = None
 
-    def init_database(self, host: str, port: str, user: str, password: str, database: str) -> SQLDatabase:
-        self.db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
-        self.engine = sqlalchemy.create_engine(self.db_uri)
-        return SQLDatabase.from_uri(self.db_uri)
+    def init_database(self, host: str, port: str, user: str, password: str, database: str):
+        db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+        engine = sqlalchemy.create_engine(db_uri)
+        return SQLDatabase.from_uri(db_uri), engine
 
     def run(self):
         with st.sidebar:
             st.subheader("Settings")
-            st.text_input("Host", value="127.0.0.1", key="Host")
-            st.text_input("Port", value="3306", key="Port")
-            st.text_input("User", value="user", key="User")
-            st.text_input("Password", type="password", value='password', key="Password")
-            st.text_input("Database", value="database", key="Database")
+            host = st.text_input("Host", value="127.0.0.1", key="Host")
+            port = st.text_input("Port", value="3306", key="Port")
+            user = st.text_input("User", value="user", key="User")
+            password = st.text_input("Password", type="password", value='password', key="Password")
+            database = st.text_input("Database", value="database", key="Database")
 
             if st.button("Conectar"):
                 with st.spinner('Conectando ao banco de dados...'):
-                    self.db = self.init_database(
-                        host=st.session_state['Host'],
-                        port=st.session_state['Port'],
-                        user=st.session_state['User'],
-                        password=st.session_state['Password'],
-                        database=st.session_state['Database'],
+                    st.session_state.db, st.session_state.engine = self.init_database(
+                        host=host,
+                        port=port,
+                        user=user,
+                        password=password,
+                        database=database,
                     )
                     st.success("Conectado ao banco de dados")
 
@@ -61,14 +62,17 @@ class ChatInterface:
                 st.markdown(user_message)
 
             with st.chat_message("AI"):
-                chain = langchain.get_sql_chain_with_openai(self.db)
-                query = chain.invoke({"question": user_message})
+                if st.session_state.db:
+                    chain = langchain.get_sql_chain_with_openai(st.session_state.db)
+                    query = chain.invoke({"question": user_message})
 
-                st.code(query)
-                df = pd.read_sql_query(query, self.engine)
-                st.dataframe(df)
+                    st.code(query)
+                    df = pd.read_sql_query(query, st.session_state.engine)
+                    st.dataframe(df)
 
-            self.chat_history.append(AIMessage(content=query))
+                    self.chat_history.append(AIMessage(content=query))
+                else:
+                    st.warning("Por favor, conecte-se ao banco de dados antes de fazer uma consulta.")
 
 
 if __name__ == "__main__":
